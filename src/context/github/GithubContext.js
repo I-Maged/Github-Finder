627408b86274e1517d225fcdf10eda9eb5a7fbb8
I-Maged <magedhanafy@gmail.com>
2022-12-01 04:25:13 +0200

    Github Reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,27 +1,38 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 import env from 'react-dotenv';
 
 const GithubContext = createContext();
 const GITHUB_URL = env.GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const [users, SetUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
+
     const response = await fetch(`${GITHUB_URL}/users`);
 
     const data = await response.json();
 
-    SetUsers(data);
-    setLoading(false);
+    dispatch({
+      type: 'GET_USERS',
+      payload: data,
+    });
   };
+
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}
     >
